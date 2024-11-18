46fe95d24061fefed0fe88a3e986d260be93bd9c
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-11-18 09:51:12 -0500

    added grandmaster/master stat files

--- backend/routes/statistics/statistics.routes.js
+++ backend/routes/statistics/statistics.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getChallengerChampionData } = require('../../controllers/statistics/challenger/champion.controller');
+const { getChallengerChampionData } = require('../../controllers/statistics/challenger/champions.controller');
 
 router.get('/:rank/champions', getChallengerChampionData);
 
