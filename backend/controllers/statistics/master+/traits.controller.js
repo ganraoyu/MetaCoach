f6d92e46351e331b8243a5b81090047a2e89dfcd
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-11-19 13:25:26 -0500

    added rank params for champion winrates

--- backend/controllers/statistics/master+/traits.controller.js
+++ backend/controllers/statistics/master+/traits.controller.js
@@ -1,9 +1,10 @@
 const traitsClient = require('../../../utils/statisticsClients/traitsClient');
 
 const getTraitsData = async (req, res) => {
+    const { rank } = req.params;
     try {
-        const getTraitsData = await traitsClient(rank);
-        res.json(getTraitsData);
+        const getTraitsRanking = await traitsClient(rank);
+        res.json(getTraitsRanking);
     } catch (error) {
         console.error('Error fetching challenger traits:', error.message);
         res.status(500).send('Error fetching challenger traits');
