9d43ea08d2939d18f72f89fe3e34abb1b09317ef
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-11-23 22:52:29 -0500

    added division for ranks

--- backend/controllers/statistics/master-/champions.controller.js
+++ backend/controllers/statistics/master-/champions.controller.js
@@ -23,4 +23,4 @@ const getBelowMasterChampionData = async (req, res) => {
     }
 }
 
-module.exports = { getBelowMasterChampionData, fetchSummonerIds }
+module.exports = { getBelowMasterChampionData }
