6ac18528c64f35bdb620548282e4ae6674159710
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-12-14 23:12:57 -0500

    added star levels

--- backend/battle-simulator/data/champions.js
+++ backend/battle-simulator/data/champions.js
@@ -1,7 +1,31 @@
 class champion {
-    constructor(name, hp, attackDamage, attackSpeed, ability, abilityDamage, range ){
+    constructor(name, hp, attackDamage, attackSpeed, ability, abilityDamage){
         this.name = name;
         this.hp = hp
-        this.attackDamage =   
+        this.attackDamage =  attackDamage;
+        this.attackSpeed = attackSpeed;
+        this.ability = ability;
+        this.abilityDamage = abilityDamage;
+        this.starLevel = 1;
+        updateStarLevel();
+    }
+
+    updateStarLevel(){
+        this.starLevel = startLevel;
+        const stats = this.statsByStarLevel[this.starLevel];
+        if(stats){
+            this.hp = stats.hp;
+            this.attackDamage = stats.attackDamage;
+            this.attackSpeed = stats.attackSpeed;
+            this.abilityDamage = stats.abilityDamage;
+        } else {
+            console.log('Invalid star level');
+        }
     }
 }
+
+const champions = [
+    new champion('Aatrox', 580, 60, 0.651, 'The Darkin Blade', 200),
+    new champion('Ahri', 526, 53, 0.668, 'Orb of Deception', 300),
+    new champion('Akali', 700, 1260, 0.625, 'Shuriken Flip', 200),
+]
