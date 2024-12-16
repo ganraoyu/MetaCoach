4e49c2629977d587436a7514b5b43bfc3ee2d465
yaoyu <115035161+YaoYaoYuYU@users.noreply.github.com>
2024-12-15 22:46:02 -0500

    added champion skeleton

--- backend/battle-simulator/data/champions.js
+++ backend/battle-simulator/data/champions.js
@@ -1,31 +1,48 @@
-class champion {
-    constructor(name, hp, attackDamage, attackSpeed, ability, abilityDamage){
+class Champion {
+    constructor(name, statsByStarLevel, attackSpeed, ability, range) {
         this.name = name;
-        this.hp = hp
-        this.attackDamage =  attackDamage;
-        this.attackSpeed = attackSpeed;
-        this.ability = ability;
-        this.abilityDamage = abilityDamage;
-        this.starLevel = 1;
-        updateStarLevel();
+        this.statsByStarLevel = statsByStarLevel; 
+        this.attackSpeed = attackSpeed; 
+        this.ability = ability; 
+        this.range = range; 
+        this.starLevel = 1; 
+        this.updateStarLevel(); 
     }
 
-    updateStarLevel(){
-        this.starLevel = startLevel;
-        const stats = this.statsByStarLevel[this.starLevel];
-        if(stats){
+    // Method to update stats based on star level
+    updateStarLevel(starLevel = this.starLevel) {
+        this.starLevel = starLevel;
+        const stats = this.statsByStarLevel[starLevel];
+        if (stats) {
+
             this.hp = stats.hp;
             this.attackDamage = stats.attackDamage;
-            this.attackSpeed = stats.attackSpeed;
+            this.armor = stats.armor;
+            this.manaResistant = stats.manaResistant;
             this.abilityDamage = stats.abilityDamage;
         } else {
-            console.log('Invalid star level');
+            console.log(`Invalid star level: ${starLevel} for ${this.name}`);
         }
     }
 }
 
+// Initialize champions
 const champions = [
-    new champion('Aatrox', 580, 60, 0.651, 'The Darkin Blade', 200),
-    new champion('Ahri', 526, 53, 0.668, 'Orb of Deception', 300),
-    new champion('Akali', 700, 1260, 0.625, 'Shuriken Flip', 200),
-]
+    new Champion(
+        'Akali',
+        {
+            1: { hp: 700, attackDamage: 45, attackSpeed: 0.75, armor: 45, manaResistant: 45, abilityDamage: 150 },
+            2: { hp: 1260, attackDamage: 90, attackSpeed: 0.75, armor: 60, manaResistant: 60, abilityDamage: 300 },
+            3: { hp: 1890, attackDamage: 135, attackSpeed: 0.75, armor: 75, manaResistant: 75, abilityDamage: 450 }
+        },
+        0.75,
+        'Shuriken Flip',
+        'Melee'
+    )
+];
+
+
+champions[0].updateStarLevel(2);
+
+
+console.log(champions[0]);
